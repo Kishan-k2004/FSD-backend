197392e129de574400507942b6189e0f4fe4ba50
Kishan-k2004 <k.khansali.2004@gmail.com>
2025-02-22 13:53:01 +0530

    day1,2,3

---
+++ day3/p2.js
@@ -0,0 +1,29 @@
+const http =require('http');
+const users=[];
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-type':'application/json'})
+    if(req.url==='/getdata' && req.method==='GET'){
+        
+        res.end(JSON.stringify(users));
+        return;
+    }
+    else if(req.url==='/setdata' && req.method==='POST'){
+        let body='';
+        req.on('data',chunk=>{
+            body+=chunk;
+        });
+
+        
+        req.on('end',()=>{
+            const data=JSON.parse(body);
+            console.log("Recieved data:",data);
+            users.push(data)
+            res.end(JSON.stringify({message:'Data recieved successfully'}))
+        });
+        return;
+    }
+    
+})
+server.listen(9000,()=>{
+    console.log('Server is running on Port 9000');
+});
